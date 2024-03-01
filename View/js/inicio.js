a34af0d8d6056b83509fe57cc69ae3f6048818c0
angelica jimenez <angelicajimenezmachacon15@gmail.com>
2024-03-01 15:10:26 -0500

    primer commit

---
+++ View/js/inicio.js
@@ -0,0 +1,6 @@
+
+let typedformat = new Typed(".publicidad", {
+    strings: ["viaja con pasion","guia con experiencia","Tu viaje, tu aventura, tu eleccion", "Redescubre el mundo con guias apasionados"],
+    loop: true,
+    typeSpeed: 100
+});
