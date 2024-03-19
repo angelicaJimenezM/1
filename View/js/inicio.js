ddf6b1e00b727994f64224eceb937503f3e76df4
Josee-cmd <josesslcedo@gmail.com>
2024-03-19 15:55:53 -0500

    Se agrego la estructura del proyecto incluyendo la instalacion de TailwindCss

--- View/js/inicio.js
+++
@@ -1,6 +0,0 @@
-
-let typedformat = new Typed(".publicidad", {
-    strings: ["viaja con pasion","guia con experiencia","Tu viaje, tu aventura, tu eleccion", "Redescubre el mundo con guias apasionados"],
-    loop: true,
-    typeSpeed: 100
-});
